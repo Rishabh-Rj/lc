836e8a56dbda2249770caaa9919257cae88a5e14
Rishabh Joshi <32004849+Rishabh-Rj@users.noreply.github.com>
2024-08-17 00:11:45 +0530

    Time: 1 ms (53.08%), Space: 42.9 MB (18.89%) - LeetHub

--- 0078-subsets/0078-subsets.java
+++ 0078-subsets/0078-subsets.java
@@ -1,30 +1,17 @@
 class Solution {
-    public List<List<Integer>> subsets;
-    public int[] nums;
     public List<List<Integer>> subsets(int[] nums) {
-        subsets = new ArrayList<>();
-        this.nums = nums;
-        
-        List<Integer> subset = new ArrayList<>();
-        backtrack(0, subset);
-        return this.subsets;
+        List<List<Integer>> res = new ArrayList<>();
+        generateSubsets(0, nums, new ArrayList<>(), res);
+        return res;
     }
-    
-    public void backtrack(int i, List<Integer> subset) {
-        if (i >= nums.length) {
-            List<Integer> ans = new ArrayList<>();
-            for(int num: subset) {
-                ans.add(num);
-            }
-            subsets.add(ans);
-        }
-        else {
-            
-            backtrack(i + 1, subset);
-            
-            subset.add(nums[i]);
-            backtrack(i + 1, subset);
-            subset.remove(subset.size() - 1);
+
+    void generateSubsets(int index, int[] nums, List<Integer> curr, List<List<Integer>> res) {
+        res.add(new ArrayList<>(curr));
+
+        for (int i = index; i < nums.length; i++) {
+            curr.add(nums[i]);
+            generateSubsets(i + 1, nums, curr, res);
+            curr.remove(curr.size() - 1);
         }
     }
 }
